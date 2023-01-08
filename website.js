b7db453091aa6e004b67a53ba443d52ef5446dec
Nicholas Cai <cainicholas91@gmail.com>
2023-01-07 20:35:25 -0500

    stuff

---
+++ website.js
@@ -0,0 +1,15 @@
+//DOM Manipulation
+
+const title = document.querySelector('#navbar');
+
+console.log(title);
+//GetElementById()
+
+//GetElementByClassName()
+
+//getElementsByTagName()
+
+//querySelector()
+// const name = document.querySelector('div'); the first case of div
+
+//querySelectorAll()
